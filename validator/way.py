f24beaf27ae8b3d13252720c0d175619907e7c2a
Nano <lucas2000bss@gmail.com>
2024-08-31 03:43:45 -0300

    Update way.py

--- validator/way.py
+++ validator/way.py
@@ -29,4 +29,4 @@ headers = {
 			                      'AppleWebKit/537.36 (KHTML, like Gecko) '
 			                      'Chrome/55.0.2883.87 Safari/537.36'
 			                      'Mozilla/5.0 (Windows NT 6.1; rv:45.0)'
-			                      'Gecko/20100101 Firefox/45.0'
+			                      'Gecko/20100101 Firefox/45.0'}
